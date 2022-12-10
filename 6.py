a1bab5a09bad588fd889b041f50cd0c97e0658a3
windows <windows@windows.com>
2022-12-11 00:00:30 +0100

    Exercise 6

---
+++ 6.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+from pathlib import Path
+
+# Bienvenida al usuario, informar ruta de acceso al directorio donde estan las 
+# recetas, informar cuantas recetas hay en total, luego pedir que elija una de estas
+# Que son estas:
+# Opción 1:
+# Preguntar que categoría elige (carnes, ensaladas, etc),
+# preguntar que receta quiere leer, mostrar su contenido
+# Opción 2:
+# Elegir una categoría, escribir el nombre y contenido de la 
+# nueva receta que va a crear, y el programa creara este archivo
+# Opción 3:
+# Elegri una categoría a crear y va a generar una carpeta con ese nombre
+# Opción 4:
+# Elegir una categoría preguntar la receta que va a eliminar
+# Opción 5:
+# Elegir que categoría quiere eliminar
+# Opción 6:
+# Finalizar la ejecución del codigo
+#
+#
+# Cuestiones 
+# Cuando acabe una de las opciones exitosamente debera presionar cualquier tecla
+# para volver al inicio
+# 
+# Limpiar la pantalla cuando se reinicie y vuelva al menu inicial
+#
+# Usar el Path para este ejercicio para la hora de leer archivos
+#
+# Usar muchas funciones 
+
+def main():
+    print ("Hola usuario")
+    Path()
+    print ("Ruta de las recetas:\n{}")
+
+
+
+if __name__=='__main__':
+    main()
